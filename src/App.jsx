16b1fe5bdf9af552218c9a51cc3dfba1eaaab64b
Eslam6104 <xedosedo12345@gmail.com>
2025-11-10 22:54:46 +0200

    Added first about section team data

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import ContactUs from "./components/ContactUs.jsx/ContactUs";
+// import ContactUs from "./components/ContactUs.jsx/ContactUs";
 import Home from "./components/HomePage/Home";
+import TeamStrategy from "./AboutUs/TeamStrategy/TeamStartegy";
 
 function App() {
   return (
     <>
       <Home />
+      <TeamStrategy/>
       {/* <ContactUs /> */}
     </>
   );
