07b726359c8b9784fcb6cc8ea82b1986fee161ce
eman-sabry <e.saad19083@gmail.com>
2025-11-20 13:54:48 +0200

    add login header

--- src/components/registerPages/SignPage.jsx
+++ src/components/registerPages/SignPage.jsx
@@ -2,59 +2,68 @@ import React, { useState } from "react";
 import { Card, CardContent, Tab, Tabs, Box } from "@mui/material";
 import Signup from "./Signup";
 import Login from "./Login";
+import HeaderOfSection from "../customHook/HeaderOfSection";
 
 const SignPage = () => {
   const [tab, setTab] = useState("login");
 
   return (
-    <Box
-      display="flex"
-      justifyContent="center"
-      alignItems="center"
-      minHeight="80vh"
-      paddingY="20px"
-    >
-      <Card
-        sx={{
-          width: 400,
-
-          boxShadow: 3,
-        }}
+    <Box>
+      <HeaderOfSection
+        title="Log in to your account"
+        breadcrumbs={[
+          { label: "Home", href: "/" },
+          tab === "login" ? { label: "Login" } : { label: "Sign Up" },
+        ]}
+      />
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        minHeight="80vh"
+        paddingY="20px"
       >
-        <Tabs
-          value={tab}
-          onChange={(e, newTab) => setTab(newTab)}
-          textColor="var(--main-color)"
-          indicatorColor="primary"
-          variant="fullWidth"
+        <Card
           sx={{
-            "& .MuiTabs-indicator": {
-              backgroundColor: "var(--main-color)", 
-              height: 3, 
-              borderRadius: 2,
-            },
-            "& .MuiTab-root": {
-             
-              fontWeight: 600,
-              color: "gray",
-            },
-            "& .Mui-selected": {
-              color: "var(--main-color)", 
-            },
+            width: 400,
+
+            boxShadow: 3,
           }}
         >
-          <Tab label="Login" value="login" />
-          <Tab label="Sign Up" value="signup" />
-        </Tabs>
+          <Tabs
+            value={tab}
+            onChange={(e, newTab) => setTab(newTab)}
+            textColor="var(--main-color)"
+            indicatorColor="primary"
+            variant="fullWidth"
+            sx={{
+              "& .MuiTabs-indicator": {
+                backgroundColor: "var(--main-color)",
+                height: 3,
+                borderRadius: 2,
+              },
+              "& .MuiTab-root": {
+                fontWeight: 600,
+                color: "gray",
+              },
+              "& .Mui-selected": {
+                color: "var(--main-color)",
+              },
+            }}
+          >
+            <Tab label="Login" value="login" />
+            <Tab label="Sign Up" value="signup" />
+          </Tabs>
 
-        <CardContent>
-          {tab === "login" ? (
-            <Login setTab={setTab} />
-          ) : (
-            <Signup setTab={setTab} />
-          )}
-        </CardContent>
-      </Card>
+          <CardContent>
+            {tab === "login" ? (
+              <Login setTab={setTab} />
+            ) : (
+              <Signup setTab={setTab} />
+            )}
+          </CardContent>
+        </Card>
+      </Box>
     </Box>
   );
 };
